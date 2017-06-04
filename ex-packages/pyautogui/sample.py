4b494974cd13017b977181bdd52a1e4eb2af0f1d
gomesuit <gomesuit@gmail.com>
2017-06-04 20:48:22 +0900

    save

---
+++ ex-packages/pyautogui/sample.py
@@ -0,0 +1,10 @@
+import pyautogui
+pyautogui.PAUSE = 1
+
+width, height = pyautogui.size()
+
+for i in range(3):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
