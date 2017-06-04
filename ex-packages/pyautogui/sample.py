6d0fbf7f6f99263550f8b3c209e451bd224bb11f
gomesuit <gomesuit@gmail.com>
2017-06-04 21:37:36 +0900

    save

--- ex-packages/pyautogui/sample.py
+++ ex-packages/pyautogui/sample.py
@@ -3,8 +3,16 @@ pyautogui.PAUSE = 1
 
 width, height = pyautogui.size()
 
-for i in range(3):
+# マウスを移動する
+for i in range(2):
     pyautogui.moveTo(100, 100, duration=0.25)
     pyautogui.moveTo(200, 100, duration=0.25)
     pyautogui.moveTo(200, 200, duration=0.25)
     pyautogui.moveTo(100, 200, duration=0.25)
+
+# マウスのポジションを取得
+print(pyautogui.position())
+
+# 座標の色を取得する
+im = pyautogui.screenshot()
+print(im.getpixel((50, 200)))
