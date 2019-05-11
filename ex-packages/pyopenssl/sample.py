c9e159a1d2cecebd03c4b2b9891129501d364fcc
gomesuit <9404411+gomesuit@users.noreply.github.com>
2019-05-11 15:31:07 +0900

    save

--- ex-packages/pyopenssl/sample.py
+++ ex-packages/pyopenssl/sample.py
@@ -1,12 +1,18 @@
 import ssl
 import OpenSSL
+import pysnooper
 
-cert = ssl.get_server_certificate(('www.google.com', 443))
-x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+@pysnooper.snoop()
+def main():
+    cert = ssl.get_server_certificate(('www.google.com', 443))
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
 
-print(u'start:', x509.get_notBefore())
-print(u'end:', x509.get_notAfter())
+    print(u'start:', x509.get_notBefore())
+    print(u'end:', x509.get_notAfter())
 
-components = x509.get_subject().get_components()
-for component in components:
-    print(component)
+    components = x509.get_subject().get_components()
+    for component in components:
+        print(component)
+
+if __name__ == "__main__":
+    main()
