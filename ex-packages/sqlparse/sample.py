b19947da2ec2b52fc6a99f36a31f2b7ab48ad0ec
gomesuit <9404411+gomesuit@users.noreply.github.com>
2021-08-29 18:54:51 +0900

    save

--- ex-packages/sqlparse/sample.py
+++ ex-packages/sqlparse/sample.py
@@ -1,11 +1,11 @@
 import sqlparse
+import pprint
 import pdb
 import rlcompleter
 
 pdb.Pdb.complete = rlcompleter.Completer(locals()).complete
 
 query = '''
-
 /* Username: Scheduled, Task ID: c0a5, Query ID: 618, Queue: scheduled_queries, Query Hash: 57d4f90c1d */ WITH TMP AS(
   SELECT
     year, month,
@@ -15,7 +15,6 @@ query = '''
   GROUP BY
     year, month
 )
-
 SELECT
   year,
   month,
@@ -39,11 +38,30 @@ print(query)
 parsed_queries = sqlparse.parse(query)
 tokens = list(parsed_queries[0].flatten())
 
+token_uniq = set()
 for t in tokens:
-    if t.ttype in sqlparse.tokens.Text.Whitespace:
-        continue
-    if t.ttype == sqlparse.tokens.Token.Punctuation:
-        continue
-    print("token_value: '{}', token_type: {}".format(t.value, t.ttype))
+    token_uniq.add(t.ttype)
+pprint.pprint(token_uniq)
+
+
+def print_tokens(tokens):
+    for t in tokens:
+        print(f"token_type: {t.ttype}, token_value: '{t.value}'")
+
+
+comments = [t for t in tokens if t.ttype in sqlparse.tokens.Comment]
+print_tokens(comments)
+
+names = [t for t in tokens if t.ttype in sqlparse.tokens.Name]
+print_tokens(names)
+
+keywords = [t for t in tokens if t.ttype in sqlparse.tokens.Keyword]
+print_tokens(keywords)
+
+iterals = [t for t in tokens if t.ttype in sqlparse.tokens.Literal]
+print_tokens(iterals)
+
+operators = [t for t in tokens if t.ttype in sqlparse.tokens.Operator]
+print_tokens(operators)
 
 pdb.set_trace()
