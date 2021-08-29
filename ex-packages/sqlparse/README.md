abe3b17fae5836f611ca8921da022d7b58c9a507
gomesuit <9404411+gomesuit@users.noreply.github.com>
2021-08-29 18:15:52 +0900

    save

--- ex-packages/sqlparse/README.md
+++ ex-packages/sqlparse/README.md
@@ -1,3 +1,6 @@
 https://ohke.hateblo.jp/entry/2020/09/19/233000
 https://github.com/andialbrecht/sqlparse
 https://sqlparse.readthedocs.io/en/latest/
+https://dev.classmethod.jp/articles/python-sqlparse-extract-not-ddl/
+https://swfz.hatenablog.com/entry/2021/02/24/201532
+https://blog.hoxo-m.com/entry/sqlparse_lex
--- ex-packages/sqlparse/sample.py
+++ ex-packages/sqlparse/sample.py
@@ -4,11 +4,46 @@ import rlcompleter
 
 pdb.Pdb.complete = rlcompleter.Completer(locals()).complete
 
-raw = "/* Username: Scheduled, Task ID: c0a5, Query ID: 618, Queue: scheduled_queries, Query Hash: 57d4f90c1d */ WITH TMP AS(\n  SELECT\n    year, month,\n    sum(blendedcost) AS cost\n  FROM\n    \"aaaaa\".\"bbbbbb\"\n  GROUP BY\n    year, month\n)\n\nSELECT\n  year,\n  month,\n  year || month as yearmonth,\n  cost \nFROM\n  TMP\norder by\n  year desc, month desc"
+query = '''
 
-print(sqlparse.format(raw, reindent=True, keyword_case='upper'))
+/* Username: Scheduled, Task ID: c0a5, Query ID: 618, Queue: scheduled_queries, Query Hash: 57d4f90c1d */ WITH TMP AS(
+  SELECT
+    year, month,
+    sum(blendedcost) AS cost
+  FROM
+    "aaaaa"."bbbbbbbb"
+  GROUP BY
+    year, month
+)
 
-statement = sqlparse.parse(raw)[0]
-statement.tokens
+SELECT
+  year,
+  month,
+  year || month as yearmonth,
+  cost
+FROM
+  TMP
+order by
+  year desc, month desc
+'''
+
+# query = sqlparse.format(
+#     query,
+#     strip_comments=True,
+#     reindent_aligned=True,
+#     keyword_case='upper',
+# )
+
+print(query)
+
+parsed_queries = sqlparse.parse(query)
+tokens = list(parsed_queries[0].flatten())
+
+for t in tokens:
+    if t.ttype in sqlparse.tokens.Text.Whitespace:
+        continue
+    if t.ttype == sqlparse.tokens.Token.Punctuation:
+        continue
+    print("token_value: '{}', token_type: {}".format(t.value, t.ttype))
 
 pdb.set_trace()
