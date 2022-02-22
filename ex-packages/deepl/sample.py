d57561e52455dc30271988b5332e1febda34ffe3
gomesuit <9404411+gomesuit@users.noreply.github.com>
2022-02-22 15:13:04 +0900

    save

--- ex-packages/deepl/sample.py
+++ ex-packages/deepl/sample.py
@@ -1,22 +1,22 @@
 import deepl
 import os
 
-# Create a Translator object providing your DeepL API authentication key.
-# To avoid writing your key in source code, you can set it in an environment
-# variable DEEPL_AUTH_KEY, then read the variable in your Python code:
 translator = deepl.Translator(os.getenv("DEEPL_AUTH_KEY"))
 
-# Translate text into a target language, in this case, French
-result = translator.translate_text("Hello, world!", target_lang="FR")
-print(result)  # "Bonjour, le monde !"
-# Note: printing or converting the result to a string uses the output text
+result = translator.translate_text(
+    "How are you?",
+    target_lang="JA",
+)
+print(result.text)
+print(result.detected_source_lang)
 
-# Translate multiple texts into British English
-result = translator.translate_text(["お元気ですか？", "¿Cómo estás?"], target_lang="EN-GB")
-print(result[0].text)  # "How are you?"
-print(result[0].detected_source_lang)  # "JA"
-print(result[1].text)  # "How are you?"
-print(result[1].detected_source_lang)  # "ES"
+result = translator.translate_text(
+    "お元気ですか？",
+    target_lang="EN-US",
+)
+
+print(result.text)
+print(result.detected_source_lang)
 
 # Check account usage
 usage = translator.get_usage()
@@ -24,15 +24,3 @@ if usage.character.limit_exceeded:
     print("Character limit exceeded.")
 else:
     print(f"Character usage: {usage.character.count} of {usage.character.limit}")
-
-# Source and target languages
-print("Source languages:")
-for language in translator.get_source_languages():
-    print(f"{language.code} ({language.name})")  # Example: "DE (German)"
-
-print("Target languages:")
-for language in translator.get_target_languages():
-    if language.supports_formality:
-        print(f"{language.code} ({language.name}) supports formality")
-    else:
-        print(f"{language.code} ({language.name})")
