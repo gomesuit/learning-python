c275db9ead3678aa07a0d508cdfeffdb4539c09d
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-09 17:12:32 +0900

    save

---
+++ class/sample_3.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self):
+        self.__private_method()
+
+    def __private_method(self):
+        print("これはプライベートメソッドです")
+
+# クラスの外部からプライベートメソッドにアクセスしようとするとエラーになる
+instance = MyClass()
+try:
+    instance.__private_method()
+except AttributeError as e:
+    print(e)  # 'MyClass' object has no attribute '__private_method'
